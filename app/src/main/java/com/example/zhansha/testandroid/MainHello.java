24785245a10239ecb3dbabd5e696ceb6709efe23
Shanwei <Shanwei.Zhang@jotautomation.com>
2019-05-10 15:50:28 +0800

    first work version

--- app/src/main/java/com/example/zhansha/testandroid/MainHello.java
+++ app/src/main/java/com/example/zhansha/testandroid/MainHello.java
@@ -26,11 +26,9 @@ public class MainHello extends AppCompatActivity {
     private static int SAMPLE_RATE_IN_HZ = 8000;
     private int number = 1;
     private int tal = 1;
-    private long currenttime;
-    private long endtime;
-    private long time = 1;
     //到达该值之后 触发事件
-    private static int BLOW_ACTIVI=3000;
+    private static int BLOW_ACTIVI=10000;
+    private static int MAX_NUMBER=2000;
 
     private Button btn;
     private TextView txtV;
@@ -50,27 +48,12 @@ public class MainHello extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (start)
-                {
-                    start = ! start;
-                    txtV.setText("You clicked Gaga!!");
-                }
-                else
-                {
-                    start = ! start;
-                    txtV.setText("Gaga is gone!!");
-                    isblow = false;
-                }
-            Moniting();
-//                if (gf_number<4)
-//                {
-//                    gf_number++;
-//                    Show();
-//
-//                }
-//                else{
-//                    gf_number=0;
-//                }
+                btn.setClickable(false);
+                btn.setActivated(false);
+                txtV.setText("aaa");
+                Moniting();
+                btn.setClickable(true);
+                btn.setActivated(true);
 
             }
         });
@@ -91,11 +74,13 @@ public class MainHello extends AppCompatActivity {
                             // check if we can instantiate and have a success
                             AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, rate, channelConfig, audioFormat, bufferSize);
 
+                            Log.e("xxxxxxxxxx", "Got available channel: "+ channelConfig);
                             if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
+                                txtV.setText("find recorder"+channelConfig);
                                 return recorder;
                         }
                     } catch (Exception e) {
-                        Log.e("main", rate + "Exception, keep trying.",e);
+                        Log.e("xxxxxxxxxx", rate + "Exception, keep trying.",e);
                     }
                 }
             }
@@ -104,48 +89,49 @@ public class MainHello extends AppCompatActivity {
     }
     public void Moniting() {
         try {
+            if (ar==null)
+            {
+                txtV.setText("ar is null");
+                gf_number = gf_number % 4 +1;
+                Show();
+                return;
+            }
             ar.startRecording();
             isblow = true;
             txtV.setText("startRecording");
             // 用于读取的 buffer
+            txtV.invalidate();
             byte[] buffer = new byte[bs];
+            boolean blowStart = false;
             while (isblow) {
                 number++;
                 sleep(8);
-                currenttime = System.currentTimeMillis();
                 int r = ar.read(buffer, 0, bs) + 1;
                 int v = 0;
-                Log.e("XXXXXXXXX", "Moniting: "+ Arrays.toString(buffer));
                 for (int i = 0; i < buffer.length; i++) {
                     v += (buffer[i] * buffer[i]);
                 }
                 int value = v / (int) r;
-                tal = tal + value;
-                endtime = System.currentTimeMillis();
-                time = time + (endtime - currenttime);
-
-                if (time >= 500 || number > 5) {
-
-                    int total = tal / number;
-                    if (total > BLOW_ACTIVI) {
-                        //发送消息通知到界面 触发动画
-                        txtV.setText("startRecording"+total);
-
-                        //利用传入的handler 给界面发送通知
-//                        BlowActivity.i+=1;
-//                        handler.sendEmptyMessage(0); //改变i的值后，发送一个空message到主线程
+                if (!blowStart && value < BLOW_ACTIVI)
+                    blowStart = true;
+                if (blowStart && (value > BLOW_ACTIVI))
+                    isblow = false;
+                txtV.setText("belowing: "+number+"ms");
+                txtV.invalidate();
+                if (number >= MAX_NUMBER) {
                         isblow=false;
-                        //
-                        number = 1;
-                        tal = 1;
-                        time = 1;
                     }
                 }
 
-            }
+            txtV.setText("endRecording: "+number);
             ar.stop();
-            ar.release();
             bs=100;
+            if (number>=600)
+                gf_number = 4;
+            else
+            gf_number = (number/200) % 4 +1;
+            //Show();
+            number = 1;
 
 
         } catch (Exception e) {
